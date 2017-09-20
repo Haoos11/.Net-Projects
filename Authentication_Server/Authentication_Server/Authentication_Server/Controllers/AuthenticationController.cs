993a5a1d4098a05b175f0205e29d0d15bdba5d02
silviu.marin <marin.silviu11@yahoo.com>
2017-09-20 16:29:39 +0300

    v1

---
+++ Authentication_Server/Authentication_Server/Authentication_Server/Controllers/AuthenticationController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Authentication_Server.Controllers
+{
+    public class User
+    {
+        public string Name;
+        public string passWord;
+
+        public User(string name, string pass)
+        {
+            this.Name = name;
+            this.passWord = pass;
+        }
+   }
+
+    public class AuthenticationController : ApiController
+    {
+        User testUser = new User("vasile", "pass");
+        
+
+        [HttpPost]
+        public IHttpActionResult Authenticate([FromBody]User user)
+        {
+            if (user.Name == testUser.Name && user.passWord == testUser.passWord)
+            {
+                var OkResponse = new HttpResponseMessage(HttpStatusCode.OK);
+                OkResponse.Headers.Add("Authorized", "Ai nimerit credentialele!");
+                return ResponseMessage(OkResponse);
+
+            }
+
+            var response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+            response.Headers.Add("Unauthorized","PAAAA!!!");
+            
+
+            return ResponseMessage(response);
+        }
+
+    }
+}
---
+++ Authentication_Server/Authentication_Server/Authentication_Server/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Authentication_Server.WebApiApplication" Language="C#" %>
